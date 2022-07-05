2da66e467d859a083f6fc5b410dda372262c194b
Eren Erinanc <73641392+erenerinanc@users.noreply.github.com>
2022-07-05 18:15:56 +0300

    VC refactored - lifecycle methods super's called

--- ViewControllerProjesi/ViewController.swift
+++ ViewControllerProjesi/ViewController.swift
@@ -13,82 +13,62 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
-    var alinanSifre = ""
+    var passcode = ""
     
-    override func viewDidLoad() { // Uygulama ilk açıldığında karşımıza çıkacak ekran ve kodlar. Herhangi bir işlemden sonra tekrar bu viewDidLoad çağrılmaz
+    override func viewDidLoad() {
+        // Uygulama ilk açıldığında karşımıza çıkacak ekran ve kodlar. Herhangi bir işlemden sonra tekrar bu viewDidLoad çağrılmaz
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         print("view did load")
+        view.backgroundColor = .systemPink
 
-        
-        
     }
-    
-    
 
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         print("view did appear")
     }
     
-    
     override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
         print("view did disappear")
     }
     
     
-    override func viewWillAppear(_ animated: Bool) { // kullanıcıdan giriş yapıp tekrar giriş alacağımız sayfaya geldiğimizde giriş alanını temizlemek için buraya yazdık.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // kullanıcıdan giriş yapıp tekrar giriş alacağımız sayfaya geldiğimizde giriş alanını temizlemek için buraya yazdık.
         print("view  will appear")
-        
         textField.text = ""
-        
     }
     
-    
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         print("view will disappear")
     }
     
     
     @IBAction func kontrolEtClicked(_ sender: Any) {
         
+        passcode = textField.text!
         
-        alinanSifre = textField.text!
-        
-        if alinanSifre == "atil" {
+        if passcode == "atil" {
             
             performSegue(withIdentifier: "toIkinciVc", sender: nil)
             
         } else {
             birinciLabel.text = "Şifreniz Yanlış"
         }
-        
-        
-        
-        
     }
-    
-    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) { // Diğer viewControllerı tanımlamak için kullanıyoruz
         
-        
         if segue.identifier == "toIkinciVc" {
             
-            
-            let destinationVc = segue.destination as! ikinciViewController // Diğer viewcontrol'a bağlanmak için casting yapmamız lazım (as)
-            destinationVc.verilenSifre = alinanSifre
-            
-            
-            
+            let destinationVc = segue.destination as! ClassIsimleriBuyukHarfleBaslar // Diğer viewcontrol'a bağlanmak için casting yapmamız lazım (as)
+            destinationVc.verilenSifre = passcode
         }
-        
-        
     }
-    
-    
-    
-    
-
 }
 
